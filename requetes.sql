be0a3651f343c5a0ecbd02e4b9e7c578cedd633d
Daphne Cluseau <daphne.cluseau@fms-ea.com>
2025-11-27 15:46:03 +0100

    modif requetes

--- requetes.sql
+++ requetes.sql
@@ -1,4 +1,8 @@
+-- -- -- -- -- -- -- -- -- -- -- -- --
+-- BDD solo
+-- -- -- -- -- -- -- -- -- -- -- -- --
 
+-- Afficher les articles comme montré dans les consignes
 SELECT stock.s_reference, article.a_designation, marque_article.b_nom, marque_voiture.mv_nom, equipe.eq_quantite, modele.m_nom, energie.e_nom
 FROM modele
 LEFT OUTER JOIN alimente ON modele.m_id = alimente.m_id
@@ -9,21 +13,16 @@ LEFT OUTER JOIN stock ON equipe.s_id = stock.s_id
 LEFT OUTER JOIN marque_article ON stock.b_id = marque_article.b_id
 LEFT OUTER JOIN article ON stock.a_id = article.a_id;
 
-
-INSERT INTO utilise
-VALUES (1, 1, 1);
-
+-- Màj quantité en stock
 UPDATE equipe
 SET eq_quantite = eq_quantite - 1
 WHERE s_id = 1;
-
-INSERT INTO utilise
-VALUES (2, 3, 2);
 
 UPDATE equipe
 SET eq_quantite = eq_quantite - 2
 WHERE s_id = 3;
 
+-- Calculer le total de la main d'oeuvre et le mettre dans la facture
 UPDATE factures
 INNER JOIN ordre_reparation ON factures.f_id = ordre_reparation.f_id
 SET f_total_main_doeuvre = ro_temps_reparation * f_tx_horaire
@@ -34,6 +33,7 @@ INNER JOIN ordre_reparation ON factures.f_id = ordre_reparation.f_id
 SET f_total_main_doeuvre = ro_temps_reparation * f_tx_horaire
 WHERE factures.f_id = 2;
 
+-- Calculer le montant total de la facture
 UPDATE factures
 INNER JOIN ordre_reparation ON ordre_reparation.f_id = factures.f_id
 INNER JOIN utilise ON utilise.ro_numero = ordre_reparation.ro_numero
@@ -48,16 +48,19 @@ INNER JOIN stock ON stock.s_id = utilise.s_id
 SET f_montant_total = f_total_main_doeuvre + (utilise.u_quantite_utilisee * stock.s_prix_unitaire)
 WHERE factures.f_id = 2;
 
+-- Calculer le montant total de toutes les factures
 SELECT f_id, SUM(f_montant_total) AS montant_total
 FROM factures
 GROUP BY f_id
 
+-- Calculer le montant total des factures par client
 SELECT c_nom, f_montant_total
 FROM client
 INNER JOIN ordre_reparation ON ordre_reparation.v_id = voiture.v_id
 INNER JOIN factures ON factures.f_id = ordre_reparation.f_id
 GROUP BY client.c_id
 
+-- Calculer la somme des quantités utilisées
 SELECT s_reference, eq_quantite AS quantite_en_stock, SUM(u_quantite_utilisee)
 FROM stock
 INNER JOIN utilise ON utilise.s_id = stock.s_id
@@ -80,11 +83,39 @@ INNER JOIN marque_article ON stock.b_id = marque_article.b_id
 INNER JOIN utilise ON utilise.s_id = stock.s_id
 GROUP BY s_reference;
 
-SELECT COUNT(s_reference) AS ref
-FROM stock
-GROUP BY s_reference;
+-- Moyenne des prix des filtres par référence
+SELECT article.a_designation, stock.s_reference, AVG(stock.s_prix_unitaire) AS moyenne_prix
+FROM article
+INNER JOIN stock ON article.a_id = stock.a_id
+WHERE article.a_designation LIKE 'Filtre%'
+GROUP BY stock.s_reference
 
--- Groupe
+-- Moyenne des prix des filtres par marque
+SELECT article.a_designation, marque_article.b_nom, AVG(stock.s_prix_unitaire) AS moyenne_prix 
+FROM article 
+INNER JOIN stock ON article.a_id = stock.a_id 
+INNER JOIN marque_article ON marque_article.b_id = stock.b_id 
+WHERE article.a_designation LIKE 'Filtre%' 
+GROUP BY marque_article.b_id; 
+
+-- Moyenne des prix des filtres par référence en utilisant les ID
+SELECT article.a_designation, stock.s_reference, AVG(stock.s_prix_unitaire) AS moyenne_prix
+FROM article
+INNER JOIN stock ON article.a_id = stock.a_id
+WHERE article.a_id IN (1, 2, 3)
+GROUP BY stock.s_reference
+
+-- Moyenne des prix des filtres par marque en utilisant les ID
+SELECT article.a_designation, marque_article.b_nom, AVG(stock.s_prix_unitaire) AS moyenne_prix 
+FROM article 
+INNER JOIN stock ON article.a_id = stock.a_id 
+INNER JOIN marque_article ON marque_article.b_id = stock.b_id 
+WHERE article.a_id IN (1, 2, 3)
+GROUP BY marque_article.b_id;
+
+-- -- -- -- -- -- -- -- -- -- -- -- --
+-- Requêtes pour le travail en groupe
+-- -- -- -- -- -- -- -- -- -- -- -- --
 
 -- articles et références
 SELECT a_ref, a_quantite
